7ec0b4426fde203f66ae9e8ad23eabd39bf1a0f5
Heath Mercer <heath.mercer3@gmail.com>
2023-10-09 20:54:03 -0500

    Create multiple Items, created a data structure to hold them

--- SFML-demo-project/Demo.cpp
+++ SFML-demo-project/Demo.cpp
@@ -1,116 +1,91 @@
 #include<SFML/Graphics.hpp>
 #include <string>
 #include <iostream>
+#include <vector>
+
+class CreateObject {
+	sf::Vector2f pos;
+	std::shared_ptr<sf::Shape> shape;
+
+public:
+	CreateObject(float pos_x, float pos_y, std::shared_ptr<sf::Shape> shapeType)
+		: pos(pos_x, pos_y), shape(shapeType) {
+		shape->setPosition(pos);
+		shape->setFillColor(sf::Color::White);
+	}
+
+	void render(sf::RenderWindow& wind) {
+		shape->setPosition(pos);
+		wind.draw(*shape);
+	}
+
+	void move(float dx, float dy) {
+		pos.x += dx;
+		pos.y += dy;
+	}
+
+	sf::Vector2f getPosition() const {
+		return pos;
+	}
+
+};
 
 int main()
 {
-	//Initialize window dimensions
 	sf::RenderWindow window(sf::VideoMode(800, 800), "Demo Window");
+	window.setFramerateLimit(60);
 
-	//Initialize array of shape objects
-	const int MAX_SHAPES = 50;
-	int number_of_shapes = 0;
-	sf::Shape *shapes[MAX_SHAPES];
-
-	//Initialize array of button objects
-	const int MAX_BUTTONS = 10;
-	int number_of_buttons = 0;
-	sf::Shape* buttons[MAX_BUTTONS];
-
-	//Create button
-	sf::RectangleShape* button1 = new sf::RectangleShape(sf::Vector2f(100.f, 50.f));
-	button1->setPosition(350, 0);
-	button1->setFillColor(sf::Color::White);
-	buttons[number_of_buttons] = button1;
-	number_of_buttons++;
+	std::vector<std::shared_ptr<CreateObject>> objects;
 
 	while (window.isOpen())
 	{
-		//Event handling
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			//Close window event
 			if (event.type == sf::Event::Closed)
 			{
 				window.close();
 			}
 
-			//Mouse click event for button click handling
-			if (event.type == sf::Event::MouseButtonPressed)
-			{
-				if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-				{
-					auto click_position = sf::Mouse::getPosition(window);
-					auto map_position = window.mapPixelToCoords(click_position);
-					if (button1->getGlobalBounds().contains(map_position))
-					{
-						//Allocates shape and store in shapes array
-						sf::CircleShape* shape3 = new sf::CircleShape(75.f);
-						shape3->setPosition(325, 325);
-						shape3->setFillColor(sf::Color::Red);
-						shapes[number_of_shapes] = shape3;
-						number_of_shapes++;
-
-					}
-				}
-
-			}
-			//Keyboard Event Handling
 			if (event.type == sf::Event::KeyPressed)
 			{
 				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1))
 				{
-					//Allocates shape and store in shapes array
-					sf::CircleShape *shape1= new sf::CircleShape(100.f);
-					shape1->setPosition(275, 275);
-					shape1->setFillColor(sf::Color::Cyan);
-					shapes[number_of_shapes] = shape1;
-					number_of_shapes++;
-
+					auto circle = std::make_shared<sf::CircleShape>(20);
+					circle->setFillColor(sf::Color::White);
+					objects.emplace_back(275, 275, circle);
 				}
 				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num2))
 				{
-					//Allocates shape and store in shapes array
-					sf::RectangleShape* shape2 = new sf::RectangleShape(sf::Vector2f(100.f, 50.f));
-					shape2->setPosition(650, 650);
-					shape2->setFillColor(sf::Color::Red);
-					shapes[number_of_shapes] = shape2;
-					number_of_shapes++;
+					auto rectangle = std::make_shared<sf::RectangleShape>(sf::Vector2f(40, 60));
+					rectangle->setFillColor(sf::Color::White);
+					objects.emplace_back(650, 650, rectangle);
+				}
 
-				}
-				//Movement event handling with arrow keys
-				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-				{
-					shapes[number_of_shapes-1]->move(0.f, 3.f);
-				}
-				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-				{
-					shapes[number_of_shapes-1]->move(0.f, -3.f);
-				}
-				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-				{
-					shapes[number_of_shapes-1]->move(-3.f, 0.f);
-				}
-				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-				{
-					shapes[number_of_shapes-1]->move(3.f, 0.f);
+				if (!objects.empty()) {
+					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+						objects.back()->move(0, -10);
+					}
+					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+						objects.back()->move(0, 10);
+					}
+					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+						objects.back()->move(-10, 0);
+					}
+					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+						objects.back()->move(10, 0);
+					}
 				}
 			}
 		}
 
 		//Display contents of object arrays in the window
 		window.clear();
-		for (int i = 0; i < number_of_buttons; i++)
-		{
-			window.draw(*buttons[i]);
-		}
-		for (int i = 0; i < number_of_shapes; i++)
-		{
-			window.draw(*shapes[i]);
+		for (auto& obj : objects) {
+			obj->render(window);
 		}
 		window.display();
-
 	}
+
 	return 0;
 }
