7f77ac466d3b9d966d4b754bd2f8c41f35252997
Heath Mercer <hem100@txtstate.edu>
2023-10-10 15:08:06 -0500

    Fixed code to allow movement, deleted gravity function
    will replace gravity function later

---
+++ ObjectInteractionDemo/ObjectInteractionDemo/Demo.cpp
@@ -0,0 +1,92 @@
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <memory>
+
+class CreateObject {
+    sf::Vector2f pos;
+    std::unique_ptr<sf::Shape> shape;
+
+public:
+    CreateObject(float pos_x, float pos_y, std::unique_ptr<sf::Shape> shapeType)
+        : pos(pos_x, pos_y), shape(std::move(shapeType)) {
+        shape->setPosition(pos);
+        shape->setFillColor(sf::Color::White);
+    }
+
+    void render(sf::RenderWindow& wind) {
+        shape->setPosition(pos);
+        wind.draw(*shape);
+    }
+
+    void move(float dx, float dy) {
+        pos.x += dx;
+        pos.y += dy;
+    }
+
+    sf::Vector2f getPosition() const {
+        return pos;
+    }
+
+};
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Demo Window");
+    window.setFramerateLimit(60);
+
+    std::vector<std::unique_ptr<CreateObject>> objects;
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1))
+                {
+                    auto circle = std::make_unique<sf::CircleShape>(20);
+                    circle->setFillColor(sf::Color::White);
+                    objects.emplace_back(std::make_unique<CreateObject>(275.0f, 275.0f, std::move(circle)));
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num2))
+                {
+                    auto rectangle = std::make_unique<sf::RectangleShape>(sf::Vector2f(40, 60));
+                    rectangle->setFillColor(sf::Color::White);
+                    objects.emplace_back(std::make_unique<CreateObject>(650.0f, 650.0f, std::move(rectangle)));
+                }
+
+                if (!objects.empty()) {
+                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+                        objects[0]->move(0, -10);
+                    }
+                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+                        objects[0]->move(0, 10);
+                    }
+                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+                        objects[0]->move(-10, 0);
+                    }
+                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+                        objects[0]->move(10, 0);
+                    }
+                }
+            }
+        }
+
+        //Display contents of object arrays in the window
+        window.clear();
+        for (auto& obj : objects) {
+            obj->render(window);
+        }
+        window.display();
+    }
+
+    return 0;
+}
