def828f04d82b5cd5f1551a445202db52db6e5dd
Alex Kaylor <glb116@txstate.edu>
2023-10-30 23:42:50 -0500

    adding of leaderboard to game form

--- Flask_Game_Host/routes.py
+++ Flask_Game_Host/routes.py
@@ -2,9 +2,29 @@
 import sys 
 sys.dont_write_bytecode = True
 from flask import Flask, render_template
+<<<<<<< SCRUM-67-code-refactoring
 from chess_engine import Engine
+=======
+from player import Player
+>>>>>>> adding of leaderboard to game form
 
 app = Flask(__name__)
+
+#temporary list of scores
+top_players = [Player("Sam", 500), Player("Jeff", 2), Player("Sally", 1000), Player("Ryan", 50), Player("Lindsay", 750)]
+#calling class method to sort scores
+top_players = Player.sort_players(top_players)
+
+def build_leaderboard_html():
+  html_string = "<table class=\"leaderboard\"><tr><th>Username</th><th>Score</th></tr>"
+  for obj in top_players:
+    html_string += "<tr><td>" + obj.username + "</td>"
+    html_string += "<td>" + str(obj.score) + "</td></tr>"
+  html_string += "</table>"
+  return html_string
+  
+  
+  
 
 @app.route('/')
 def homepage():
@@ -16,7 +36,8 @@ def about():
 
 @app.route('/game')
 def game():
-  return render_template('game.html')
+  leaderboard = build_leaderboard_html()
+  return render_template('game.html', leaderboard_game1=leaderboard)
 
 @app.route('/move/<int:depth>/<path:fen>/')
 def get_move(depth, fen):
