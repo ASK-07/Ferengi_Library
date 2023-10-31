a9d1ece2fb717f3a059be6d4f1b8f746e7a35a01
Jaime Flores <jaf289@txstate.edu>
2023-10-31 16:17:03 -0500

    Added all of the Sprint 1 Prototypes into Sprint1_Project

---
+++ Sprint1_Project/PhysicsEngine/src/Dynamics/DynamicsWorld.h
@@ -0,0 +1,14 @@
+//This class creates our physics objects and defines the force, gravity, and the step function
+//to determine the current position of our object
+#include "RigidObject.h"
+
+class DynamicsWorld
+{
+    //creates our object in the physics world
+    void createObject(RigidObject* newObject);
+
+    //method to remove our object in the physics world
+
+    
+
+};
