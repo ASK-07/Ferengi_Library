04cb9cf1f7b9e10b07f06d0cd2791b29d5c52fde
nkf17 <nkf17@txstate.edu>
2023-11-04 10:58:44 -0500

    renames file, adds href anchor to titles and images, condenses code

--- Flask_Game_Host/html_generator.py
+++ Flask_Game_Host/html_generator.py
@@ -1,19 +1,17 @@
-import games_dictionary
+from games import gameDict
 
-def fill_default_grid():
+def fill_grid():
     '''
     Uses dictionary of games to create HTML string containing aggregate of game-cell divs
 
-    Grid style can be modified in homepage.html
-
     '''
     # Get dictionary storing game info
-    gamesDictionary = games_dictionary.gameDict
+    gamesDictionary = gameDict
 
     # Initialize HTML string of game-cell divs to empty
     game_cells = ''
 
-    # Build each cell of game grid using dictionary
+    # Build each cell of grid using dictionary
     for game in gamesDictionary.keys():
 
         # Fetch info about current game
@@ -21,21 +19,19 @@ def fill_default_grid():
         game_image = gamesDictionary[game][1]
         game_description = gamesDictionary[game][2]
 
-        # Create html string for current game's image
+        # Create open anchor for game's page
+        game_anchor = '<a href="/' + game + '">'
+
+        # Create html string that loads current game's image
         image_string = '<img src="img/' + game_image + '" alt="' + game_image + '"/>'
 
         # Keeping this here to remember the pain
         # image_string = '<img src="{{ url_for(\'static\', filename=\'' + game_image + '\') }}" alt="' + game_image + '" class="image" />'
 
-        # Initialize current game-cell div
-        game_cells += '<div class="game-cell">'
-
-        # Turn current game's info into complete game-cell div
-        game_cells += image_string
-        game_cells += '<br><u>'
-        game_cells += game_title
-        game_cells += '</u><br>'
-        game_cells += game_description
-        game_cells += '</div>'
+        # Turn current game into game-cell div
+        game_cells += '<div class="game-cell">'                             # Open game-cell div
+        game_cells += game_anchor + image_string + '</a><br>'               # Add anchored image
+        game_cells += game_anchor + '<u>' + game_title + '</u></a><br>'     # Add anchored title
+        game_cells += game_description + '</div>'                           # Add description, close div
     
     return game_cells
