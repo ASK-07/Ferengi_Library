ce891ee591cc021641af5cdd59e7457cd1a0ce09
nkf17 <nkf17@txstate.edu>
2023-11-13 23:28:35 -0600

    Abstracts initialization, run files from routes.py

--- Flask_Game_Host/routes.py
+++ Flask_Game_Host/routes.py
@@ -1,14 +1,17 @@
-#!/usr/bin/env python -B
-import sys 
-sys.dont_write_bytecode = True
-from flask import Flask, render_template
-from chess_engine import Engine
-from player import Player
-from game import Game
-from html_generator import fill_grid
+'''
+    This module defines the flask app's route decorators
 
-app = Flask(__name__, static_url_path='', static_folder='static',template_folder='templates')
+'''
+from Flask_Game_Host import app
+from flask import render_template
+from Flask_Game_Host.html_generator import fill_grid
+from Flask_Game_Host.chess_engine import Engine
+from Flask_Game_Host.player import Player
+from Flask_Game_Host.game import Game
 
+
+
+# <can be moved to html_generator once MongoDB API implemented>
 #temporary list of scores
 top_players = [Player("Sam", 500), Player("Jeff", 2), Player("Sally", 1000), Player("Ryan", 50), Player("Lindsay", 750)]
 #temporary list of game titles to use for scoreboard
@@ -25,20 +28,27 @@ def build_leaderboard_html():
   html_string += "</table>"
   return html_string
 
+
+
+
+
 @app.route('/')
 @app.route('/home')
 def homepage():
-  game_grid = fill_grid()
-  return render_template('homepage.html', grid_display=game_grid)
+    game_grid = fill_grid()
+    return render_template('homepage.html', grid_display=game_grid)
+
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
+
 
 @app.route('/chess')
 def chess():
   leaderboard = build_leaderboard_html()
   return render_template('chess.html', leaderboard_chess=leaderboard)
+
 
 @app.route('/move/<int:depth>/<path:fen>/')
 def get_move(depth, fen):
@@ -50,13 +60,16 @@ def get_move(depth, fen):
     print()
     return move
 
+
 @app.route('/test/<string:tester>')
 def test_get(tester):
     return tester
-    
+
+
 @app.route('/pinball')
 def play_pinball():
     return render_template('pinball.html')
+
 
 #@app.route('/save_high_scores', methods=['POST'])
 #def save_high_scores():
@@ -71,7 +84,4 @@ def play_pinball():
      #   file.write(high_score)
 
    # return redirect('/')
-
-
-if __name__ == '__main__':
-  app.run(debug=True)
+   
