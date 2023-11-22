068a6c91cbc9831fb452d90e1ecaff246c823f21
Heath Mercer <heath.mercer3@gmail.com>
2023-11-21 22:00:48 -0600

    Removed Sprint 1 Files, etc.

--- Flask_Game_Host/__init__.py
+++ Flask_Game_Host/__init__.py
@@ -13,16 +13,6 @@ app.config["MONGO_URI"] = 'mongodb+srv://Heath:VsaClEbzM7VkaUuQ@dev-aws-website.
 
 mongo.init_app(app)
 
-score_data = [
-    {'player_name' : "Heath",'score' : 12000},
-    {'player_name' : "Jake",'score' : 14000},
-    {'player_name' : "Nathan",'score' : 15000},
-    {'player_name' : "Alex",'score' : 15000},
-    {'player_name' : "Jaime",'score' : 15001}
-]
-
-mongo.db.HostedGames.insert_many(score_data)
-
 from .routes import routes_app
 app.register_blueprint(routes_app)
 
