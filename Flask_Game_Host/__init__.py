420353faf2257ea3ee7537271251603a001e7738
nkf17 <nkf17@txstate.edu>
2023-11-25 14:29:03 -0600

    Reconfigures app extensions

--- Flask_Game_Host/__init__.py
+++ Flask_Game_Host/__init__.py
@@ -1,13 +1,26 @@
-from flask import Flask, app
-from .mongo_config import mongo
-
+import os
+from flask import Flask, app        # Ensure no bugs from importing app
+# from .mongo_config import mongo   # Unsure if necessary, works without
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
-app = Flask(__name__, static_url_path='', static_folder='static',template_folder='templates')
+app = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates')
 
-app.config["MONGO_URI"] = 'mongodb+srv://Heath:VsaClEbzM7VkaUuQ@dev-aws-website.ppvg8ly.mongodb.net/Games?retryWrites=true&w=majority'
+# Configure flask app extensions
+load_dotenv()
+uri = os.getenv('MONGO_URI')
 
-mongo.init_app(app)
+app.config['MONGO_URI'] = uri
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+# Put key in .env/ sys env var
+# app.config["MONGO_URI"] = 'mongodb+srv://Heath:VsaClEbzM7VkaUuQ@dev-aws-website.ppvg8ly.mongodb.net/Games?retryWrites=true&w=majority'
+
+# Establish connection to database
+mongodb_client = MongoClient(uri)
+db = mongodb_client['Games']
+
+# mongo.init_app(app)
 
 score_data = [
     {'player_name' : "Heath",'score' : 12000},
@@ -17,9 +30,11 @@ score_data = [
     {'player_name' : "Jaime",'score' : 15001}
 ]
 
-mongo.db.HostedGames.insert_many(score_data)
+# This adds 5 documents to HostedGames every time app is ran... There are 700 documents in the collection
+# Also consider changing collection name to Scores
+# mongo.db.HostedGames.insert_many(score_data)
 
 from .routes import routes_app
 app.register_blueprint(routes_app)
 
-from Flask_Game_Host import routes
+# from Flask_Game_Host import routes    # Likely unnecessary
