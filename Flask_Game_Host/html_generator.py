ff9c10e52247474ea004bde53b3f018c57698535
nkf17 <nkf17@txstate.edu>
2023-11-27 13:50:18 -0600

    Removes description from generator

--- Flask_Game_Host/html_generator.py
+++ Flask_Game_Host/html_generator.py
@@ -26,10 +26,9 @@ def fill_grid():
         image_string = '<img src="img/' + game_image + '" alt="' + game_image + '"/>'
 
         # Turn current game into game-cell div
-        game_cells += '<div class="game-cell">'                             # Open game-cell div
-        game_cells += game_anchor + image_string + '</a><br>'               # Add anchored image
-        game_cells += game_anchor + '<u>' + game_title + '</u></a><br>'     # Add anchored title
-        game_cells += game_description + '</div>'                           # Add description, close div
+        game_cells += '<div class="game-cell">'                                     # Open game-cell div
+        game_cells += game_anchor + image_string + '</a><br>'                       # Add anchored image
+        game_cells += game_anchor + '<u>' + game_title + '</u></a><br></div>'       # Add anchored title, close div
     
     return game_cells
 
